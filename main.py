1b19de70fd8b7256277b230c387eb424a15f1792
disilva021P <disilva.profissional@gmail.com>
2024-04-24 18:28:18 +0100

    24/04

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ from libgen_api import LibgenSearch
 
 
 
-def pesquisaLivro(pesquisa):
+def pesquisaLivro(pesquisa,filtros):
     s = LibgenSearch()
-    results = s.search_title(pesquisa)
-    print(results)
+    if filtros:
+        results = s.search_title_filtered(pesquisa,filtros,exact_match=True)
+    else:
+        results = s.search_title(pesquisa)
+    input("print, download?"))
+    urlretrieve(url, filename)
 
-    
+
 if __name__ == "__main__":
-    pesquisaLivro("Pride and Prejudice")
+    filtros={
+
+    }
+    pesquisa = input("Nome do Livro:")
+    ano = input("Qual ano?")
+    ext = input("Extensão:")
+    if ano:
+        filtros["Year"] = ano
+    if ext:
+        filtros["Extension"] = ext
+    print(filtros)
+    
+    pesquisaLivro(pesquisa,filtros)
